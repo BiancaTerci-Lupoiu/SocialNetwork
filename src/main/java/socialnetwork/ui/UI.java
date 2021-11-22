d6f39e0fa171a71d19a7b8cc38fa0d23ec9194e2
Terec Andrei <driver71528@gmail.com>
2021-11-22 09:10:56 +0200

    Am adaugat posibilitatea de a adauga argumente inline care contin spatii scriindu-le intre ghilimele

--- src/main/java/socialnetwork/ui/UI.java
+++ src/main/java/socialnetwork/ui/UI.java
@@ -11,9 +11,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.time.format.DateTimeParseException;
-import java.util.Arrays;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 public abstract class UI {
     private final Map<String, UIFunction> commands;
@@ -55,6 +53,27 @@ public abstract class UI {
         return inputReader.readLine();
     }
 
+    private String[] splitArguments(String line)
+    {
+        String[] quoteSeparated = line.split("\"",-1);
+        System.out.println(Arrays.toString(quoteSeparated));
+        List<String> args = new ArrayList<>();
+        for(int i=0;i< quoteSeparated.length;i++)
+        {
+            if(quoteSeparated[i].trim().equals(""))
+                continue;
+            if(i%2==0)
+                args.addAll(Arrays.stream(quoteSeparated[i].split(" "))
+                        .map(String::trim)
+                        .filter(x-> !x.equals(""))
+                        .toList());
+            else
+                args.add(quoteSeparated[i]);
+        }
+        System.out.println(args);
+        return args.toArray(String[]::new);
+    }
+
     /**
      * starts the application
      */
@@ -63,7 +82,8 @@ public abstract class UI {
             try {
                 System.out.print(">>>");
                 String commandLine = inputReader.readLine();
-                String[] args = commandLine.split(" ");
+
+                String[] args = splitArguments(commandLine);
                 String command = args[0];
                 var function = commands.get(command);
                 if (function == null)
