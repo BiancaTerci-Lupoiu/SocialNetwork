dafe55c6fe060fd604c21dfd0bef102da43df88f
Terec Andrei <driver71528@gmail.com>
2021-12-12 14:52:29 +0200

    am adaugat loaderul in factory

---
+++ src/main/java/project/lab6/setter_interface/SetterService.java
@@ -0,0 +1,7 @@
+package project.lab6.setter_interface;
+
+import project.lab6.service.Service;
+
+public interface SetterService {
+    void setService(Service service);
+}
--- src/main/java/project/lab6/utils/Constants.java
+++ src/main/java/project/lab6/utils/Constants.java
@@ -8,4 +8,13 @@ public class Constants {
     public static DateTimeFormatter DATETIME_FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
     public static DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy");
     public static Color DEFAULT_CHAT_COLOR = Color.MAGENTA;
+
+    public static class View
+    {
+        public static String CREATE_NEW_ACCOUNT="views/createNewAccount.fxml";
+        public static String FRIENDS = "views/friends.fxml";
+        public static String LOGIN = "views/login.fxml";
+        public static String MAIN_VIEW="views/main-view.fxml";
+        public static String REQUESTS="views/requests.fxml";
+    }
 }
