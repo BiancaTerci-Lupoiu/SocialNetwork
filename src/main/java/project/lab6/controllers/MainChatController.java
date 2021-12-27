5bb717e3f14f0a9e6d0f607c7a95e295276392e5
Bianca Terci-Lupoiu <bianca.terciu@gmail.com>
2021-12-28 00:44:57 +0200

    Am adaugat functia de setConversationView in MainChatController, am inceput sa lucrez la fereastra Conversation

--- src/main/java/project/lab6/controllers/MainChatController.java
+++ src/main/java/project/lab6/controllers/MainChatController.java
@@ -1,16 +1,26 @@
 package project.lab6.controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
+import javafx.util.Callback;
+import project.lab6.domain.chat.Chat;
 import project.lab6.domain.dtos.ChatDTO;
 import project.lab6.factory.Factory;
 import project.lab6.service.ServiceMessages;
 import project.lab6.setter_interface.SetterIdLoggedUser;
 import project.lab6.setter_interface.SetterServiceMessages;
+import project.lab6.utils.Constants;
+
+import java.io.IOException;
 
 public class MainChatController implements SetterServiceMessages, SetterIdLoggedUser {
 
@@ -51,11 +61,24 @@ public class MainChatController implements SetterServiceMessages, SetterIdLogged
     @FXML
     public HBox mainHorizontalBox;
     @FXML
-    public ListView listViewChats;
+    public ListView<ChatDTO> listViewChats;
 
-
+    ObservableList<ChatDTO> chatDTOList= FXCollections.observableArrayList();
     private ServiceMessages serviceMessages;
     private Long idLoggedUser;
+
+    public void initialize(){
+        chatDTOList.setAll(serviceMessages.getChatsDTO(idLoggedUser));
+        listViewChats.setItems(chatDTOList);
+        listViewChats.setCellFactory(new Callback<ListView<ChatDTO>, ListCell<ChatDTO>>() {
+            @Override
+            public ListCell<ChatDTO> call(ListView<ChatDTO> param) {
+                return new CustomCellChat(idLoggedUser);
+            }
+        });
+        //setConversationView(chatDTOList.get(0).);
+    }
+
     /**
      * sets the id of the logged user to idLoggedUser
      *
@@ -69,5 +92,23 @@ public class MainChatController implements SetterServiceMessages, SetterIdLogged
     @Override
     public void setServiceMessages(ServiceMessages serviceMessages) {
         this.serviceMessages=serviceMessages;
+    }
+
+    public void setConversationView(Long idChat){
+        FXMLLoader loader=Factory.getInstance().getLoader(Constants.View.CONVERSATION);
+        ConversationController conversationController=loader.getController();
+        conversationController.setIdChat(idChat);
+
+        Region region = null;
+        try {
+            region = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (mainHorizontalBox.getChildren().size() > 1)
+            mainHorizontalBox.getChildren().set(1, region);
+        else
+            mainHorizontalBox.getChildren().add(region);
+        HBox.setHgrow(region, Priority.ALWAYS);
     }
 }
