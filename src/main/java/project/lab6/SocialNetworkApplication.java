d68451e8c82b801c109f85cc7d7a14ad6570f59a
Tudor Decea <tudor.decea@stud.ubbcluj.ro>
2021-12-29 20:03:07 +0200

    am schimbat SocialNetworkApplication pentru a da merge

--- src/main/java/project/lab6/SocialNetworkApplication.java
+++ src/main/java/project/lab6/SocialNetworkApplication.java
@@ -4,10 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import project.lab6.controllers.CreateGroupController;
 import project.lab6.factory.Factory;
-import project.lab6.service.ServiceFriends;
-import project.lab6.service.ServiceMessages;
 import project.lab6.utils.Constants;
 
 import java.io.IOException;
@@ -15,8 +12,9 @@ import java.io.IOException;
 public class SocialNetworkApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = Factory.getInstance().getLoader(Constants.View.LOGIN);
+        FXMLLoader fxmlLoader = Factory.getInstance().getLoader(null, Constants.View.LOGIN);
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
