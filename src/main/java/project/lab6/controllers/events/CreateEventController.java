db1e5589abbe5215c3930059fe5a0b5c8de7543e
Bianca Terci-Lupoiu <bianca.terciu@gmail.com>
2022-01-10 17:49:14 +0200

    Am adaugat la create event sa poti introduce data si ora la care are loc evenimentul

--- src/main/java/project/lab6/controllers/events/CreateEventController.java
+++ src/main/java/project/lab6/controllers/events/CreateEventController.java
@@ -1,19 +1,18 @@
 package project.lab6.controllers.events;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.util.converter.LocalDateStringConverter;
 import project.lab6.controllers.AlertMessage;
 import project.lab6.controllers.Controller;
 import project.lab6.controllers.MainViewController;
 import project.lab6.domain.validators.ValidationException;
 import project.lab6.service.ServiceEvents;
-import project.lab6.service.ServiceFriends;
 import project.lab6.utils.Constants;
 
+import java.sql.Time;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class CreateEventController extends Controller {
 
@@ -28,6 +27,14 @@ public class CreateEventController extends Controller {
     public TextField titleTextField;
     @FXML
     public Button createEventButton;
+    @FXML
+    public DatePicker datePicker;
+    @FXML
+    public Spinner<Integer> minutesSpinner;
+    @FXML
+    public Spinner<Integer> hoursSpinner;
+    @FXML
+    public Button backToProfileButton;
 
     public CreateEventController(ServiceEvents serviceEvents, Long idLoggedUser, MainViewController mainViewController) {
         this.serviceEvents = serviceEvents;
@@ -35,6 +42,14 @@ public class CreateEventController extends Controller {
         this.mainViewController = mainViewController;
     }
 
+    public void initialize(){
+        SpinnerValueFactory<Integer> valueFactoryHours =
+                new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 23, 0);
+        SpinnerValueFactory<Integer> valueFactoryMinutes =
+                new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 59, 0);
+        minutesSpinner.setValueFactory(valueFactoryMinutes);
+        hoursSpinner.setValueFactory(valueFactoryHours);
+    }
     @Override
     public String getViewPath() {
         return Constants.View.CREATE_EVENT;
@@ -42,11 +57,17 @@ public class CreateEventController extends Controller {
 
     public void createEvent() {
         try {
-            serviceEvents.createEvent(idLoggedUser, titleTextField.getText(), descriptionTextArea.getText(), LocalDateTime.now());
+            LocalTime time=LocalTime.of(hoursSpinner.getValue(),minutesSpinner.getValue());
+            LocalDateTime dateTime=LocalDateTime.of(datePicker.getValue(),time);
+            serviceEvents.createEvent(idLoggedUser, titleTextField.getText(), descriptionTextArea.getText(), dateTime);
             mainViewController.openProfileView();
         }catch(ValidationException validationException)
         {
             AlertMessage.showErrorMessage(validationException.getMessage());
         }
         }
+
+    public void backToProfileAction() {
+        mainViewController.openProfileView();
+    }
 }
