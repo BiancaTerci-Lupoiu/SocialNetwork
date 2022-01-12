c502c5aecdf9be859095fb40551644c6f62bb315
Terec Andrei <driver71528@gmail.com>
2022-01-12 11:56:15 +0200

    Am adaugat clasa DateTimePicker

--- src/main/java/project/lab6/utils/Constants.java
+++ src/main/java/project/lab6/utils/Constants.java
@@ -8,30 +8,30 @@ import java.time.format.DateTimeFormatter;
  * class for declared constants
  */
 public class Constants {
-    public static DateTimeFormatter DATETIME_FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
-    public static DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-    public static Color DEFAULT_CHAT_COLOR = Color.MAGENTA;
+    public static final DateTimeFormatter DATETIME_FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
+    public static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+    public static final Color DEFAULT_CHAT_COLOR = Color.MAGENTA;
 
     public static class View {
-        public static String ADD_FRIENDS = "views/friends/addFriends.fxml";
-        public static String FRIENDS = "views/friends/friends.fxml";
-        public static String REQUESTS = "views/friends/requests.fxml";
+        public static final String ADD_FRIENDS = "views/friends/addFriends.fxml";
+        public static final String FRIENDS = "views/friends/friends.fxml";
+        public static final String REQUESTS = "views/friends/requests.fxml";
 
-        public static String CREATE_NEW_ACCOUNT = "views/login/createNewAccount.fxml";
-        public static String LOGIN = "views/login/login.fxml";
+        public static final String CREATE_NEW_ACCOUNT = "views/login/createNewAccount.fxml";
+        public static final String LOGIN = "views/login/login.fxml";
 
-        public static String ADD_GROUP_MEMBER = "views/messages/addGroupMember.fxml";
-        public static String CHAT_DETAILS = "views/messages/chatDetails.fxml";
-        public static String MAIN_CHAT = "views/messages/chatMain.fxml";
-        public static String CONVERSATION = "views/messages/conversation.fxml";
-        public static String CREATE_NEW_GROUP = "views/messages/createGroup.fxml";
-        public static String OPEN_PRIVATE_CHAT = "views/messages/openPrivateChat.fxml";
+        public static final String ADD_GROUP_MEMBER = "views/messages/addGroupMember.fxml";
+        public static final String CHAT_DETAILS = "views/messages/chatDetails.fxml";
+        public static final String MAIN_CHAT = "views/messages/chatMain.fxml";
+        public static final String CONVERSATION = "views/messages/conversation.fxml";
+        public static final String CREATE_NEW_GROUP = "views/messages/createGroup.fxml";
+        public static final String OPEN_PRIVATE_CHAT = "views/messages/openPrivateChat.fxml";
 
-        public static String MAIN_VIEW = "views/main-view.fxml";
-        public static String PROFILE = "views/profile.fxml";
+        public static final String MAIN_VIEW = "views/main-view.fxml";
+        public static final String PROFILE = "views/profile.fxml";
 
-        public static String CREATE_EVENT = "views/events/createEvent.fxml";
-        public static String EVENTS = "views/events/events.fxml";
+        public static final String CREATE_EVENT = "views/events/createEvent.fxml";
+        public static final String EVENTS = "views/events/events.fxml";
 
     }
 }
